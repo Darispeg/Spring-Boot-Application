ba45937882014e824fc6d2fbde05de73c48c651d
Darispeg <darispegdennis@gmail.com>
2021-04-18 22:14:07 -0400

    Primer Commit

---
+++ src/main/java/com/example/ApplicationFull/ApplicationFullApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApplicationFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApplicationFull/ApplicationFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApplicationFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
